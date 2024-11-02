c26ce450b3bce710a08254d8ecc3f8d4a56fa3dc
Ubuntu <ubuntu@ip-172-31-17-74.us-east-2.compute.internal>
2024-11-02 23:06:28 +0000

    first-playbook

--- adhock.sh
+++
@@ -1,7 +0,0 @@
-- ansible -i inventory all -m "shell" -a "nproc"
-
-- ansible -i inventory all -m "shell" -a "df -h"
-
-- ansible -i inventory all -m "shell" -a "free -g"
-
-- ansible -i inventory all -m "shell" -a "touch test"
